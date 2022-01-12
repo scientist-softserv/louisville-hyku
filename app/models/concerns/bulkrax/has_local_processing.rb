9c67b3cfcdf6fa2063889391a653388f6e142d2b
summer-cook <summercook@bellsouth.net>
2022-01-12 09:36:19 -0800

    fix has local processing file- change it to module and add missing end

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 module Bulkrax
-  class HasLocalProcessing
-  # This method is called during build_metadata
-  # add any special processing here, for example to reset a metadata property
-  # to add a custom property from outside of the import data
-  def add_local; end
+  module HasLocalProcessing
+    # This method is called during build_metadata
+    # add any special processing here, for example to reset a metadata property
+    # to add a custom property from outside of the import data
+    def add_local; end
+  end
 end
