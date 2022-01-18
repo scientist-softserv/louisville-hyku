588dd7f3f2add7f5cdc362623d511a7f813848f8
Shana <shana.lavina.moore@gmail.com>
2022-01-17 16:13:39 -0800

    fix for a few failing specs

--- spec/services/create_jpg_service_spec.rb
+++ spec/services/create_jpg_service_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 RSpec.describe CreateJpgService, clean: true do
   let(:pdf_file) do
     create(:uploaded_file,
-           file: File.open(::Rails.root.join('spec/fixtures/pdf/archive.pdf')))
+           file: File.open(::Rails.root.join('spec/fixtures/pdf/cat_scan.pdf')))
   end
   let(:user) { FactoryBot.create(:admin) }
 
   it 'takes in a pdf and returns a file with jpgs for each page' do
     files = CreateJpgService.new([pdf_file], user).create_jpgs
     expect(files).not_to include(pdf_file)
-    expect(files.count).to eq 6
+    expect(files.count).to eq 1
   end
 end
