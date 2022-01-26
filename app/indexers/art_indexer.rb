4770b52c0b36634c31bbd8ba14d4396b606e47d5
Rob Kaufman <rob@notch8.com>
2022-01-26 14:32:09 -0800

    remove bad commit

--- app/indexers/art_indexer.rb
+++ app/indexers/art_indexer.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD:app/indexers/art_indexer.rb
 # Generated via
 #  `rails generate hyrax:work Art`
-class ArtIndexer < Hyrax::WorkIndexer
-=======
-class AppIndexer < Hyrax::WorkIndexer
->>>>>>> d787fca048e7563d4376fe3df05e4e71ffbec378:app/indexers/app_indexer.rb
+class ArtIndexer < AppIndexer
   # This indexes the default metadata. You can remove it if you want to
   # provide your own metadata and indexing.
   include Hyrax::IndexesBasicMetadata
