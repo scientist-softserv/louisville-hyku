16580c60f4978c45dbced16ffa48d55127eadee6
Alisha Evans <alisha@notch8.com>
2022-02-04 10:31:51 -0600

    move total_pages override to work show presenter decorator so it is available to all work types

--- app/presenters/hyrax/text_presenter.rb
+++ app/presenters/hyrax/text_presenter.rb
@@ -6,13 +6,5 @@ module Hyrax
   class TextPresenter < Hyku::WorkShowPresenter
     # Adds behaviors for hyrax-doi plugin.
     # include Hyrax::DOI::DOIPresenterBehavior
-
-    # OVERRIDE: Hyrax 2.9.6
-    def total_pages(members = nil)
-      # if we're hiding the derivatives, we won't have as many pages to show
-      pages = members.presence || total_items
-
-      (pages.to_f / rows_from_params.to_f).ceil
-    end
   end
 end
