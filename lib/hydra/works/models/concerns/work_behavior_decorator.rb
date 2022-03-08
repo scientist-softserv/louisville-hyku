cfd4f0b0465c63a5c2d76038f170a8f421d46fdc
Alisha Evans <alisha@notch8.com>
2022-03-08 13:54:03 -0600

    fix indention

--- lib/hydra/works/models/concerns/work_behavior_decorator.rb
+++ lib/hydra/works/models/concerns/work_behavior_decorator.rb
@@ -5,11 +5,11 @@
 module Hydra
   module Works
     module WorkBehaviorDecorator
-        # OVERRIDE hydra-works v.1.2.0
-        # we need the child works to be in the order they're listed in the csv
-        def child_works
-          ordered_works + member_works
-        end
+      # OVERRIDE hydra-works v.1.2.0
+      # we need the child works to be in the order they're listed in the csv
+      def child_works
+        ordered_works + member_works
+      end
     end
   end
 end
