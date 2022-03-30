000a5df3baa474363a4f89d6d5d405e6992a1891
Shana <shana.lavina.moore@gmail.com>
2022-03-29 17:59:14 -0700

    i90- if ssl is configured, use https

--- app/presenters/hyrax/iiif_manifest_presenter.rb
+++ app/presenters/hyrax/iiif_manifest_presenter.rb
@@ -19,8 +19,8 @@ module Hyrax
     delegate_all
 
     def search_service
-      Rails.application.routes.url_helpers.solr_document_iiif_search_url(id,
-                                                                         host: hostname)
+      url = Rails.application.routes.url_helpers.solr_document_iiif_search_url(id, host: hostname)
+      ENV['HYKU_SSL_CONFIGURED'] == 'true' ? url.sub(/\Ahttp:/, 'https:') : url
     end
 
     ##
