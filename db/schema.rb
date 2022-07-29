1787665ff29e6f3926454c99d83c269c46e1f7b1
rkuehn-uofl <randy.kuehn@louisville.edu>
2022-07-29 11:49:56 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2022_03_07_164617) do
     t.datetime "last_succeeded_at"
     t.string "importerexporter_type", default: "Bulkrax::Importer"
     t.integer "import_attempts", default: 0
+    t.index ["importerexporter_id"], name: "index_bulkrax_entries_on_importerexporter_id"
   end
 
   create_table "bulkrax_exporter_runs", force: :cascade do |t|
