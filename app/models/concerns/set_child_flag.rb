8f9589fbfea9424bb46cc0fab9fda5750ad8a554
Rob Kaufman <rob@notch8.com>
2022-08-24 10:29:30 -0700

    use env var for root url

--- app/models/concerns/set_child_flag.rb
+++ app/models/concerns/set_child_flag.rb
@@ -6,15 +6,14 @@ module SetChildFlag
     after_save :set_children
 
     property :is_child,
-             predicate: ::RDF::URI.intern('https://hyku.library.louisville.edu/terms/isChild'),
-             multiple: false do |index|
-      index.as :stored_searchable
-    end
+      predicate: ::RDF::URI.intern('https://#{ENV.fetch('HYKU_ROOT_HOST', "hyku.library.louisville.edu")}/terms/isChild'),
+      multiple: false do |index|
+        index.as :stored_searchable
+      end
   end
 
   def set_children
     ordered_works.each do |child_work|
-      child_work.update(is_child: true) unless child_work.is_child
-    end
+    child_work.update(is_child: true) unless child_work.is_child
   end
 end
