9c65153d12c95419e389ebb989b9d34b2ccd70ca
rkuehn-uofl <randy.kuehn@louisville.edu>
2022-10-20 12:06:52 -0400

    Update hyku_mailer.rb

--- app/mailers/hyku_mailer.rb
+++ app/mailers/hyku_mailer.rb
@@ -3,7 +3,7 @@
 # Provides a default host for the current tenant
 class HykuMailer < ActionMailer::Base
   def default_url_options
-    { host: ENV.fetch('HYKU_ROOT_HOST', "hyku.library.louisville.edu") }
+    { host: ENV.fetch('HYKU_ROOT_HOST', nil) }
   end
 
   private
