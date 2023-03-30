6cb437118465ce98204950f7fd833d06dad1eca3
April Rieger <april.rieger@yahoo.com>
2023-03-29 22:21:05 -0700

    Error on staging with Hyrax.config, seeing that iiif uses ActiveFedora.id_field

--- app/search_builders/hyrax/collection_member_search_builder_decorator.rb
+++ app/search_builders/hyrax/collection_member_search_builder_decorator.rb
@@ -31,9 +31,8 @@ module Hyrax
 
     # join from file id to work relationship solrized file_set_ids_ssim
     def join_for_works_from_files
-      "{!join from=#{Hyrax.config.id_field} to=file_set_ids_ssim}#{dismax_query}"
+      "{!join from=#{ActiveFedora.id_field} to=file_set_ids_ssim}#{dismax_query}"
     end
-  end
 end
 
 Hyrax::CollectionMemberSearchBuilder.prepend(Hyrax::CollectionMemberSearchBuilderDecorator)
