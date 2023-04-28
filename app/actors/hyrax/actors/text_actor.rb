ff7254312cc1ee98396a0274d5e9d0da016caf79
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2023-04-28 16:40:56 -0700

    sanitize :file_set_ids_to_restore in all work actors

--- app/actors/hyrax/actors/text_actor.rb
+++ app/actors/hyrax/actors/text_actor.rb
@@ -5,7 +5,11 @@
 module Hyrax
   module Actors
     class TextActor < Hyrax::Actors::BaseActor
-      # TODO: documentation
+      # TODO: Remove after Fedora data migration
+      # Prevent :file_set_ids_to_restore from being passed to
+      # BaseActor#apply_save_data_to_curation_concern. It is not a
+      # metadata property and is meant to be only used when migrating Fedora.
+      # @see lib/tasks/migrate_fedora.rake
       def clean_attributes(attributes)
         clean_attrs = super
         clean_attrs.except(:file_set_ids_to_restore)
