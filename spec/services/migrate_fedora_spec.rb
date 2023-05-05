607c784f3837e7d07144d48f47a1e68c61c1157f
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2023-05-05 15:33:29 -0700

    tests for #migrate!
    Co-authored-by: Braydon Justice <sephirothkod@users.noreply.github.com>

--- spec/services/migrate_fedora_spec.rb
+++ spec/services/migrate_fedora_spec.rb
@@ -7,6 +7,50 @@ RSpec.describe MigrateFedora, type: :service do
     FileUtils.rm('tmp/migrate_fedora.log')
   end
 
+  describe '#migrate!' do
+    before do
+      allow(Hyrax::CollectionType).to receive(:find_or_create_default_collection_type)
+      allow(Hyrax::CollectionType).to receive(:find_or_create_admin_set_type)
+      allow(AdminSet).to receive(:find_or_create_default_admin_set_id)
+    end
+
+    it 'calls #create_default_collection_types' do
+      expect(migrate_fedora_service).to receive(:create_default_collection_types)
+
+      migrate_fedora_service.migrate!
+    end
+
+    it 'calls #create_default_admin_set' do
+      expect(migrate_fedora_service).to receive(:create_default_admin_set)
+
+      migrate_fedora_service.migrate!
+    end
+
+    it 'calls #migrate_works' do
+      expect(migrate_fedora_service).to receive(:migrate_works)
+
+      migrate_fedora_service.migrate!
+    end
+
+    it 'calls #migrate_collections' do
+      expect(migrate_fedora_service).to receive(:migrate_collections)
+
+      migrate_fedora_service.migrate!
+    end
+
+    it 'calls #restore_relationships' do
+      expect(migrate_fedora_service).to receive(:restore_relationships)
+
+      migrate_fedora_service.migrate!
+    end
+
+    it 'calls #handle_errors' do
+      expect(migrate_fedora_service).to receive(:handle_errors)
+
+      migrate_fedora_service.migrate!
+    end
+  end
+
   describe '#initialize' do
     it 'creates a log file at tmp/migrate_fedora.log' do
       allow(Hyrax::CollectionType).to receive(:find_or_create_default_collection_type)
